61e0385aa1940e2b7b6b163d309edf21ceb1f62b
Joott <jonathan.ott1994@gmail.com>
2019-05-15 16:27:25 +0200

    Load dlls in fsx

--- src/CellCounter/Counter.fsx
+++ src/CellCounter/Counter.fsx
@@ -1,5 +1,13 @@
 ﻿
 #r @"C:\Users\Student\source\repos\CellCounter\src\CellCounter\bin\Release\netstandard2.0\CellCounter.dll"
+#r @"C:\Users\Student\source\repos\CellCounter\src\CellCounter\bin\Release\net47\FSharpAux.dll"
+#r @"C:\Users\Student\source\repos\CellCounter\src\CellCounter\bin\Release\net47\FSharp.Stats.dll"
+#r @"C:\Users\Student\source\repos\CellCounter\src\CellCounter\bin\Release\net47\FSharp.Plotly.dll"
+#r @"C:\Users\Student\source\repos\CellCounter\src\CellCounter\bin\Release\net47\System.Xaml.dll"
+#r @"C:\Users\Student\source\repos\CellCounter\src\CellCounter\bin\Release\net47\PresentationCore.dll"
+#r @"C:\Users\Student\source\repos\CellCounter\src\CellCounter\bin\Release\net47\WindowsBase.dll"
+#r @"C:\Users\Student\source\repos\BioFSharp\bin\BioFSharp.ImgP\net47\BioFSharp.ImgP.dll"
+#r @"C:\Users\Student\source\repos\CellCounter\src\CellCounter\bin\Release\netstandard2.0\CellCounter.dll"
 
 open CounterFunctions
 
